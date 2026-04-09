ab67d047bd19edab0c84cb328bd7a341c2aa3363
Nathan Ngau <Tur44631@temple.edu>
2026-04-09 10:30:08 -0400

    spelling typo

--- src/main/java/dev/emmie/Reward.java
+++ src/main/java/dev/emmie/Reward.java
@@ -4,6 +4,6 @@ package dev.emmie;
 // let the user claim and create awwards 
 public interface Reward {
   void claim_reward();
-  void create_award(String award_name, String award_description);
+  void create_reward(String award_name, String award_description);
 }
 
