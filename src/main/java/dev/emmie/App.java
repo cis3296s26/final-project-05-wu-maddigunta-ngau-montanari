720ec06225a8f3226034b0d92dd433177f585053
emolucy <lucy.accts@gmail.com>
2026-04-09 16:34:33 -0400

    add test rectangle that can be dragged

--- src/main/java/dev/emmie/App.java
+++ src/main/java/dev/emmie/App.java
@@ -1,9 +1,12 @@
 package dev.emmie;
 
+import javafx.scene.shape.Rectangle;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -11,19 +14,47 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    double offsetX;
+    double offsetY;
+
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        // create root and scene
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 900, 600);
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        // create questmap pane
+        Pane questMap = new Pane();
+
+        // create quest rectangle
+        Rectangle testRect = new Rectangle(0, 0, 30, 30);
+        testRect.setFill(Color.TAN);
+        testRect.setStroke(Color.BLACK);
+
+        // handlers for dragging
+        testRect.setOnMousePressed(event -> {
+            offsetX = event.getSceneX() - testRect.getLayoutX();
+            offsetY = event.getSceneY() - testRect.getLayoutY();
+        });
+
+        testRect.setOnMouseDragged(event -> {
+            testRect.setLayoutX(event.getSceneX() - offsetX);
+            testRect.setLayoutY(event.getSceneY() - offsetY);
+        });
+
+        // add test rectangle to questmap
+        questMap.getChildren().add(testRect);
+
+        // put questmap in root pane
+        root.setCenter(questMap);
+
+        stage.setTitle("IRL Quest Book");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
