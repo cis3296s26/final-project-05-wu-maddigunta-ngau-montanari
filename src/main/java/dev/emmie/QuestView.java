2098bbf4b983c07e0298604b17ae5f4927c75d0a
emolucy <lucy.accts@gmail.com>
2026-04-10 10:26:33 -0400

    add tooltips

--- src/main/java/dev/emmie/QuestView.java
+++ src/main/java/dev/emmie/QuestView.java
@@ -1,15 +1,18 @@
 package dev.emmie;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import javafx.scene.paint.Color;
 
 public class QuestView extends StackPane {
     private double offsetX;
     private double offsetY;
     private Rectangle rect;
+    private boolean isDragging = false;
 
-    public QuestView(Quest quest) {
+    public QuestView(Quest quest, Label tooltip) {
         // create quest rectangle
         this.rect = new Rectangle(0, 0, 30, 30);
         rect.setFill(Color.TAN);
@@ -20,6 +23,8 @@ public class QuestView extends StackPane {
 
         // handlers for dragging
         this.setOnMousePressed(event -> {
+            isDragging = true;
+            tooltip.setVisible(false);
             offsetX = event.getSceneX() - this.getLayoutX();
             offsetY = event.getSceneY() - this.getLayoutY();
         });
@@ -27,6 +32,31 @@ public class QuestView extends StackPane {
         this.setOnMouseDragged(event -> {
             this.setLayoutX(event.getSceneX() - offsetX);
             this.setLayoutY(event.getSceneY() - offsetY);
+        });
+
+        this.setOnMouseReleased(event -> {
+            isDragging = false;
+        });
+
+        // handlers for tooltip config
+        this.setOnMouseEntered(event -> {
+            tooltip.setText(quest.getName());
+            tooltip.toFront();
+            if (!isDragging) {
+                tooltip.setVisible(true);
+            }
+        });
+
+        this.setOnMouseExited(event -> {
+            tooltip.setVisible(false);
+        });
+
+        this.setOnMouseMoved(event -> {
+            tooltip.setLayoutX(event.getSceneX());
+            tooltip.setLayoutY(event.getSceneY() - 20);
+            if (!isDragging) {
+                tooltip.setVisible(true);
+            }
         });
     }
 }
