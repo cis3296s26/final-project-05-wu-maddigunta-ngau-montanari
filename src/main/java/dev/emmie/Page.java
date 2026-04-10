f7f1332612e5f4d1a3e3387db5c7f9818659c7da
Nathan Ngau <Tur44631@temple.edu>
2026-04-10 12:19:17 -0400

    constructor for the page, page name setter and getter

--- src/main/java/dev/emmie/Page.java
+++ src/main/java/dev/emmie/Page.java
@@ -1,5 +1,26 @@
 package dev.emmie;
+import java.util.List;
 
 public class Page {
-  
+
+	// list of questlines that are on this page
+	private List<Questline> questlines;
+	private String title;
+
+	// constructor to initialize the page object
+	public void createPage(List<Questline> questlines, String title) {
+		this.questlines = questlines;
+		this.title = title;
+	}
+
+	// method to return the title of the page
+	public String pageName(String title) {
+		return title;
+	}
+
+	// method to set the title of the page
+	public String setPageName(String title) {
+		this.title = title;
+		return title;
+	}
 }
