2456e97e2b8599bcd4cd2a1a9876fb36b1d0063c
emolucy <lucy.accts@gmail.com>
2026-04-10 10:26:33 -0400

    add tooltips

--- src/main/java/dev/emmie/App.java
+++ src/main/java/dev/emmie/App.java
@@ -2,6 +2,7 @@ package dev.emmie;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,10 +21,17 @@ public class App extends Application {
         // create questmap pane
         Pane questMap = new Pane();
 
-        // add test rectangle to questmap
-        Quest quest = new Quest("test", "test", false);
-        QuestView questView = new QuestView(quest);
-        questMap.getChildren().add(questView);
+        // create label
+        Label tooltip = new Label();
+        questMap.getChildren().add(tooltip);
+
+        // add two test quests with arrow between
+        Quest quest1 = new Quest("test1", "test1", false);
+        Quest quest2 = new Quest("test2", "test2", false);
+        QuestView questView1 = new QuestView(quest1, tooltip);
+        QuestView questView2 = new QuestView(quest2, tooltip);
+        questMap.getChildren().add(questView1);
+        questMap.getChildren().add(questView2);
 
         // put questmap in root pane
         root.setCenter(questMap);
