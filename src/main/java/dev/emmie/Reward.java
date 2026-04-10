e303282c65fbedb61190a77804f9d50e2c7548cb
HenryWu7175 <wuh7175@gmail.com>
2026-04-09 20:19:31 -0400

    fixed indent

--- src/main/java/dev/emmie/Reward.java
+++ src/main/java/dev/emmie/Reward.java
@@ -2,7 +2,7 @@ package dev.emmie;
 
 
 // let the user claim and create rewards 
-public interface Reward {
-  void claim_reward(UserProfile user);
-  void create_reward(String reward_name, String reward_description);
+public interface Reward {  
+    void claim_reward(UserProfile user);
+    void create_reward(String reward_name, String reward_description);
 }
