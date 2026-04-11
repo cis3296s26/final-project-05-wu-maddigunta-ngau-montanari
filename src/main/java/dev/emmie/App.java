6cd222c4802c8d7fc9a83c1555901fb5879b21ef
emolucy <lucy.accts@gmail.com>
2026-04-10 21:15:33 -0400

    connect questlines visually

--- src/main/java/dev/emmie/App.java
+++ src/main/java/dev/emmie/App.java
@@ -21,16 +21,21 @@ public class App extends Application {
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 900, 600);
 
-        // add two test quests with arrow between
+        // add some test quests with arrow between
         Quest quest1 = new Quest("test1", "test1", false);
         Quest quest2 = new Quest("test2", "test2", false);
+        Quest quest3 = new Quest("test3", "test3", false);
+        Quest quest4 = new Quest("test4", "test4", false);
 
-        quest1.addNextQuest(quest2);
-        quest2.addPrerequisite(quest1);
+        quest1.addPrerequisite(quest2);
+        quest2.addPrerequisite(quest3);
+        quest2.addPrerequisite(quest4);
 
         Questline questline = new Questline("test");
         questline.addQuest(quest1);
         questline.addQuest(quest2);
+        questline.addQuest(quest3);
+        questline.addQuest(quest4);
 
         // create PageView pane
         List<Questline> questlines = new ArrayList<>();
