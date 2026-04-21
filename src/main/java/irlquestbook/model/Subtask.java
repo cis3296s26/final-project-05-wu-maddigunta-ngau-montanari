3fcd17d3675034c0353a8b0a56322e46c9356d53
Nathan Ngau <Tur44631@temple.edu>
2026-04-21 12:06:28 -0400

    Cosumer added to addSubtask in quest.java, changed checkComplete in quest.java, subtask.java now has a setter for the onComplete consumer, a callback for when subtask is marked complete, and set complete method. the callback checks if the subtasks are complete.

--- src/main/java/irlquestbook/model/Subtask.java
+++ src/main/java/irlquestbook/model/Subtask.java
@@ -1,8 +1,11 @@
 package irlquestbook.model;
 
+import java.util.function.Consumer;
+
 public class Subtask {
     private String title;
     private boolean completed;
+    private Consumer<Subtask> onComplete; // callback for when the subtask is marked as complete
 
     public Subtask(String title) {
         this.title = title;
@@ -17,8 +20,18 @@ public class Subtask {
         return completed;
     }
 
+    // setter for the onComplete cosumer, callback to trigger when the subtask is marked as complete
+    public void setOnComplete(Consumer<Subtask> onComplete) {
+        this.onComplete = onComplete;
+    }
+    
     public void setCompleted(boolean set) {
-        completed = set;
-        // TODO: trigger consumer
+        boolean wasCompleted = this.completed;
+        this.completed = set;
+        
+        // trigger consumer, check if the subtask was not previously completed and is now marked as complete
+        if (!wasCompleted && this.completed && onComplete != null) {
+            onComplete.accept(this);
+        }
     }
 }
