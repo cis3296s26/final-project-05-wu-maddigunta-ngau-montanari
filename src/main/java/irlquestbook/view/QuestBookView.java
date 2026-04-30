f43f143b5ae7e5f310a3ebe6da4ded7774d7f462
Emmie <98668340+emolucy@users.noreply.github.com>
2026-04-30 13:30:18 -0400

    add icons and add stackpane layer to questbookview

--- src/main/java/irlquestbook/view/QuestBookView.java
+++ src/main/java/irlquestbook/view/QuestBookView.java
@@ -1,4 +1,5 @@
 package irlquestbook.view;
+
 import irlquestbook.model.*;
 
 import java.util.function.Consumer;
@@ -6,12 +7,18 @@ import java.util.HashMap;
 
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 public class QuestBookView extends BorderPane {
     private HashMap<Page, PageView> pages;
+    private StackPane stackPane;
+    private PageView currentPage;
 
     public QuestBookView(QuestBook qb, Consumer<Quest> onQuestClick) {
         this.pages = new HashMap<>();
+
+        this.stackPane = new StackPane();
+        this.setCenter(stackPane);
 
         // create pageviews for pages and add to map
         for (Page page : qb.getPages()) {
@@ -23,7 +30,13 @@ public class QuestBookView extends BorderPane {
         }
 
         // create sidebar
-        SidebarView sb = new SidebarView(qb, page -> this.setCenter(pages.get(page)));
+        SidebarView sb = new SidebarView(qb, page -> {
+            PageView newPage = pages.get(page);
+            stackPane.getChildren().remove(currentPage);
+            stackPane.getChildren().add(0, newPage);
+            currentPage = newPage;
+        });
+
         sb.prefWidthProperty().bind(this.widthProperty().multiply(0.25));
         sb.setMinWidth(Region.USE_PREF_SIZE);
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module irlquestbook {
     requires transitive javafx.graphics;
     requires transitive javafx.base;
     requires com.google.gson;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.feather;
 
     exports irlquestbook;
     exports irlquestbook.model;
