b12b371709d1e0f88a54d9371249576db8f8284f
Henry Wu <143651504+HenryWu7175@users.noreply.github.com>
2026-05-01 21:56:10 -0400

    Revert "Feature/editadd"

--- src/main/java/irlquestbook/view/QuestBookView.java
+++ src/main/java/irlquestbook/view/QuestBookView.java
@@ -1,14 +1,14 @@
 package irlquestbook.view;
 
-import java.util.HashMap;
+import irlquestbook.model.*;
+
 import java.util.function.Consumer;
 
 import org.kordamp.ikonli.feather.Feather;
 import org.kordamp.ikonli.javafx.FontIcon;
 
-import irlquestbook.model.Page;
-import irlquestbook.model.Quest;
-import irlquestbook.model.QuestBook;
+import java.util.HashMap;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ToggleButton;
